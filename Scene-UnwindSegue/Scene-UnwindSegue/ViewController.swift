555a30ec793ccdb8cdc757e56f20361c0be0e851
JaeungHyun <jaeung@me.com>
2018-05-08 01:23:35 +0900

    practice manual segue and unwind

---
+++ Scene-UnwindSegue/Scene-UnwindSegue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scene-UnwindSegue
+//
+//  Created by JaeUng Hyun on 08/05/2018.
+//  Copyright © 2018 JaeUng Hyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func unwindToVC(_ segue: UIStoryboardSegue) {
+        
+        }
+
+
+}
+

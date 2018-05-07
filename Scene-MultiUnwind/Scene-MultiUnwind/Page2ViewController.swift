99678e7545a18dc7fe60550df701d9cae8165558
JaeungHyun <jaeung@me.com>
2018-05-08 01:42:09 +0900

    Back to page2 scene

---
+++ Scene-MultiUnwind/Scene-MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Page2ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by JaeUng Hyun on 08/05/2018.
+//  Copyright © 2018 JaeUng Hyun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Page2ViewController: UIViewController {
+    
+    @IBAction func gotoPage2(_ segue: UIStoryboardSegue) {
+        
+    }
+}

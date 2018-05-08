32c85e3931272fcece20787d9818335a00f71bef
JaeungHyun <jaeung@me.com>
2018-05-08 14:39:31 +0900

    chapter4 finished

---
+++ Scene-CustomSegue/Scene-CustomSegue/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Scene-CustomSegue
+//
+//  Created by JaeUng Hyun on 08/05/2018.
+//  Copyright © 2018 JaeUng Hyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "custom_segue") {
+            NSLog("커스텀 세그가 실행됩니다")
+        } else if (segue.identifier == "action_segue") {
+            NSLog("Action segue is implemented")
+        } else {
+            NSLog("Unknown segue")
+        }
+    }
+}
+

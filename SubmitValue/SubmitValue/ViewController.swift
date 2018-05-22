78ac631b74e0ea3774d29f487957506d991fbce7
JaeungHyun <jaeung@me.com>
2018-05-22 11:09:24 +0900

    until 324pages

--- SubmitValue/SubmitValue/ViewController.swift
+++ SubmitValue/SubmitValue/ViewController.swift
@@ -35,6 +35,29 @@ class ViewController: UIViewController {
         self.intervalText.text = "\(value)분 마다"
     }
     
+    @IBAction func onPerformSegue(_ sender: Any) {
+        self.performSegue(withIdentifier: "ManualSubmit", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // read destination viewcontroller
+        let dest = segue.destination
+        
+        guard let rvc = dest as? ResultViewController else {
+            return
+        }
+        
+        rvc.paramEmail = self.email.text!
+        rvc.paramUpdate = self.isUpdate.isOn
+        rvc.paramInterval = self.interval.value
+        
+        //    1. 메소드의 매개변수 segue의 속성 destination를 이용하여 목적지 뷰 컨트롤러의 인스턴스 참조를 가져온다.
+        //    2. 인스턴스 타입을 UIViewController에서 ResultController 타입으로 캐스팅한다. 실패하면 매소드 실행 종료
+        //    3. 캐스팅된 인스턴스 상수 rvc를 이용하여 값 전달
+    }
+
+    
     @IBAction func onSubmit(_ sender: Any) {
         // VC2 생성
         // 속성 변수를 참조할 수 없으므로 ResultViewController 타입으로 캐스팅해야함 타입을 캐스팅하면 변수 참조 가능
@@ -47,8 +70,7 @@ class ViewController: UIViewController {
         rvc.paramEmail = self.email.text!
         rvc.paramUpdate = self.isUpdate.isOn
         rvc.paramInterval = self.interval.value
-        
-        self.present(rvc, animated: true)
+        self.navigationController?.pushViewController(rvc, animated: true)
         
     }
     
